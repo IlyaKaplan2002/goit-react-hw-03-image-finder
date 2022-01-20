541d809c2a1871d96b142f6324e279b44e8554d5
Kaplan Ilya <kaplan11092002@gmail.com>
2022-01-20 13:10:52 +0200

    image finder done

---
+++ src/Components/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Modal from './Modal';
+import Loader from './Loader';
+import Button from './Button';
+import api from '../api/api';
+
+class App extends Component {
+  state = {
+    images: [],
+    modalImage: null,
+    query: '',
+    page: 1,
+    isModalOpen: false,
+    isLoading: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
+      api
+        .getImages(this.state.query, this.state.page)
+        .then(images => this.setState(prev => ({ images: [...prev.images, ...images] })))
+        .catch(console.log)
+        .finally(() => this.setState({ isLoading: false }));
+    }
+  }
+
+  onSearchSubmit = query => {
+    if (query === this.state.query) return;
+
+    this.setState({ query: query, page: 1, images: [], isModalOpen: false, isLoading: true });
+  };
+
+  onLoadMore = () => {
+    this.setState(prev => ({ page: prev.page + 1, isLoading: true }));
+  };
+
+  onImageClick = largeImage => this.setState({ isModalOpen: true, modalImage: largeImage });
+
+  onCloseModal = () => this.setState({ isModalOpen: false });
+
+  render = () => {
+    const { isLoading, isModalOpen, query, images, modalImage } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.onSearchSubmit} />
+        <ImageGallery images={images} onImageClick={this.onImageClick} />
+        {isModalOpen && <Modal image={modalImage} onCloseModal={this.onCloseModal} />}
+        {isLoading && <Loader />}
+        {query && <Button onClick={this.onLoadMore} />}
+      </>
+    );
+  };
+}
+
+export default App;
---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '24624766-a407a9b2d63732b27ccf63dc7';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const getImages = (query, page) => {
+  return axios
+    .get(
+      `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12
+`,
+    )
+    .then(res => res.data.hits);
+};
+
+export default { getImages };
